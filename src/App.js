54c63ca657cb302456665e01789ce11145c84805
souravb786 <55682234+souravb786@users.noreply.github.com>
2021-10-02 19:32:43 +0530

    Login Page commit
    this commit include the login page and the react router

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import Header from "./Components/Header"
 import Home from "./Pages/Home"
+import Login from './Pages/Login';
+import { BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch } from "react-router-dom"
 function App() {
   return (
     <div className="App">
-      <Home/>
+      {/* <Home/> */}
+      
+      <Router >
+        <Switch>
+          <Route component={Home} exact path="/"/>
+          <Route component={Login} exact path="/login"/>  
+        </Switch>  
+      </Router>
     </div>
   );
 }
