c8013b296986775cb67220e5c8a6f582edadd5e7
Vikhyat Yadav <vikhyat19127@iiitd.ac.in>
2021-10-13 13:45:06 +0530

    Product card on top of checkout page(fixed error)

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,39 +1,43 @@
-import React from 'react'
-import Header from '../Components/Header'
-import Banner from "../Components/Banner"
-import "../Styles/Home.css"
+import React from "react";
+import Header from "../Components/Header";
+import Banner from "../Components/Banner";
+import ProductCard from "../Components/ProductCard";
+
+import "../Styles/Home.css";
 function Home() {
-    
-    return (
-        <div className="Home">
-            <div className="home-header">
-                <Header />
-            </div>
-            {/* Banner */}
-            <div className="home-banner">
-                <Banner />
-                {/* <Banner />
+  return (
+    <div className="Home">
+      <div className="home-header">
+        <Header />
+      </div>
+      {/* Banner */}
+      <div className="home-banner">
+        <Banner />
+        {/* <Banner />
                 <Banner /> */}
-            </div>
-            <div>Hello</div>
-            <div>Hello</div>
-            <div>Hello</div>
-            <div>Hello</div>
-            <div>Hello</div>
-            <div>Hello</div>
-            <div>Hello</div>
-            <div>Hello</div>
-            <div>Hello</div>
-            <div>Hello</div>
-            <div>Hello</div>
-            <div>Hello</div>
-            <div>Hello</div>
-            <div>Hello</div>
-            <div>Hello</div>
-            <div>Hello</div>
-            <div>Hello</div>
-        </div>
-    )
+      </div>
+      <div className="product">
+        <ProductCard />
+      </div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+      <div>Hello</div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
